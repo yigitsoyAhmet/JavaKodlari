32114eff444b1b72c8aabc7a335cd6b252222626
AhmetYigitsoy <ahmetyigitsoy92@gmail.com>
2023-04-27 16:14:40 +0300

    scannerandDataCasting

---
+++ src/day01_dataTurleri_Scanner/C05_ScannerIlkGorev.java
@@ -0,0 +1,15 @@
+package day01_dataTurleri_Scanner;
+
+import java.util.Scanner;
+
+public class C05_ScannerIlkGorev {
+
+    public static void main(String[] args) {
+        // kullanicidan bir sayi alin
+        // ve sayinin karesini yazdirin
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Lutfen bir sayi girin");
+        double girilenSayi= scan.nextDouble();
+        System.out.println("Girilen sayinin karesi : " + girilenSayi*girilenSayi);
+    }
+}
