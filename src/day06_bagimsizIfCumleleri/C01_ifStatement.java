11ff28a11ef524a9be2740d00bb8114d66f3ee76
AhmetYigitsoy <ahmetyigitsoy92@gmail.com>
2023-05-03 19:57:03 +0300

    6.gun sonu

---
+++ src/day06_bagimsizIfCumleleri/C01_ifStatement.java
@@ -0,0 +1,30 @@
+package day06_bagimsizIfCumleleri;
+
+public class C01_ifStatement {
+    public static void main(String[] args) {
+        /*
+            Buradaki sartlar birbirinden bagimsizdir
+            uygun sayilar olursa 3 if body'si de calisabilir
+            bazen de hicbir if body'si devreye girmeyebilir
+
+            kodumuz konsolda hicbir sey yazdirmadi
+            ancak
+            Process finished with exit code 0
+            yazmasindan kodun hicbir sorun olmadan calistigini anliyoruz
+
+            sadece degerler sartlari saglamadigi icin
+            konsolda bir sey yazdirilmadi
+         */
+        int a = 13;
+        int b = 97;
+        if (a>b){
+            System.out.println("a b'den buyuk");
+        }
+        if (a%2==0){
+            System.out.println("a cift sayidir");
+        }
+        if (b>100){
+            System.out.println("b 100'den buyuktur");
+        }
+    }
+}
